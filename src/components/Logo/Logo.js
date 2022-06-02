3897cba8921f6b13ae13740f8688456a7895b47d
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-06-02 16:55:32 +0000

    Added Logo.js

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import { Container, LogoImg, LogoNameContainer } from './Logo.style';
+import logo from "../../assets/logo-light.svg";
+
+const Logo = ({ makeNameVisible, logoWidth, nameSize }) => {
+  return (
+    <Container>
+      <LogoImg logoWidth={logoWidth} src={logo} alt="Wolib logo" />
+      {makeNameVisible ? (
+        <LogoNameContainer nameSize={nameSize} >
+              Wonder Libray <br />
+              <span>Ministries</span>
+        </LogoNameContainer>
+      ) : null}
+    </Container>
+  );
+};
+
+export default Logo;
