d40bce2adbd9a341c870042769a769ef132b65b2
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-06-02 16:51:22 +0000

    Added CartIcon.style.js

---
+++ src/components/CartIcon/CartIcon.style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  width: max-content;
+  margin: 0 0 0 auto;
+`;
+export const CartImg = styled.img`
+  width: ${(props) => props.cartImgWidth}em;
+  aspect-ratio: 1;
+`;
+
+export const Badge = styled.div`
+  position: absolute;
+  inset: 0% -14% auto auto;
+
+  display: grid;
+  place-content: center;
+  width: 2.5ch;
+  padding: 0 0.9em;
+  background-color: ${(props) => props.theme.red};
+  border-radius: 100%;
+
+  font-size: 0.6em;
+  font-weight: 700;
+  line-height: 100%;
+
+  aspect-ratio: 1;
+`;
