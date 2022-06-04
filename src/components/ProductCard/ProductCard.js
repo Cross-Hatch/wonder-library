3751a0f82e5d32182b13a2747db490ddc42a31d8
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-06-04 21:42:32 +0000

    Added ProductCard.js

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,28 @@
+import {
+  Container,
+  ProductDescription,
+  ProductImg,
+  ProductOverlay,
+} from './ProductCard.style';
+import ProductCartIcon from './ProductCartIcon/ProductCartIcon';
+import { stripHtml } from 'string-strip-html';
+
+const Product = ({ product }) => {
+  const description = stripHtml(product.description).result;
+
+  return (
+    <Container image={product.image?.url}>
+      {/* <ProductImg src={image} /> */}
+      <ProductOverlay>
+        <ProductDescription>
+          <h3>{product.name}</h3>
+          <hr />
+          {description}
+        </ProductDescription>
+      </ProductOverlay>
+      <ProductCartIcon color={'#111'} />
+    </Container>
+  );
+};
+
+export default Product;
