ced33c512c5c6f932de38b858edea3c7afeffda5
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-06-04 05:55:47 +0000

    [Modified] App.js
    Added
    - Theme Provider
    - Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./components/GlobalStyles";
+import {theme} from "./components/ThemeProvider"
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Store from './pages/Store/Store'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/store" element={<Store/>} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
