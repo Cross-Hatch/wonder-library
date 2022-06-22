1225a76bc3158595d9075456c7d71945a15f8afb
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-06-22 09:08:26 +0000

    Added useProduct.js

---
+++ src/hooks/useProduct.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import commerce from '../lib/commerce';
+
+const useProduct = ({limit = "", query = ""}) => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProducts = () => {
+    commerce.products
+      .list({
+        limit: limit,
+        query: query
+      })
+      .then((products) => {
+        setProducts(products.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log('There was an error fetching the products', error);
+      });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+console.log(products);
+  return { loading, products };
+};
+
+export default useProduct;
