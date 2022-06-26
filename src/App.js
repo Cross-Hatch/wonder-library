d3eb17aebb67ba5798c356f40ad2a70f8ec2f35e
GideonAg <gideonagbosu72@gmail.com>
2022-06-26 04:32:37 +0000

    Mdified App.js
    Added Login component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./components/GlobalStyles";
-import {theme} from "./components/ThemeProvider"
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Store from './pages/Store/Store'
+import { theme } from "./components/ThemeProvider";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Store from "./pages/Store/Store";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle/>
+      <GlobalStyle />
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="/store" element={<Store/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </ThemeProvider>
