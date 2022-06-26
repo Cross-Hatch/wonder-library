a370b7f8ec8786c8d8eb5a0e2212acf6461ade95
GideonAg <gideonagbosu72@gmail.com>
2022-06-26 04:33:03 +0000

    Added Login.js

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,39 @@
+import { Container, Form, Input } from "./Login.style";
+import commerce from "../../lib/commerce";
+import React from "react";
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (email) =>
+    commerce.customer
+      .login(`${email}`, "http://localhost:3000/login/callback")
+      .then((token) => {
+        console.log(token);
+      });
+
+  React.useEffect(() => {
+    window.addEventListener("submit", (event) => {
+      event.preventDefault();
+      if (email && email.trim() !== "") {
+        handleSubmit(email);
+      }
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <p>Login</p>
+        <Input
+          onChange={(event) => setEmail(event.target.value)}
+          placeholder="Enter your email"
+          type={"email"}
+        />
+      </Form>
+    </Container>
+  );
+}
+
+export default Login;
