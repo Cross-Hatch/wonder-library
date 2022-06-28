26e136dc7ce448d15d79bf28b6b5a5777c8b3b8a
GideonAg <gideonagbosu72@gmail.com>
2022-06-28 17:13:22 +0000

    Modified Login.style.js

--- src/pages/Login/Login.style.js
+++ src/pages/Login/Login.style.js
@@ -1,7 +1,44 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 40vw;
+  height: 45vh;
+  margin: 6vh auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  border-radius: 10px;
+  box-shadow: 0 0 3px;
+`;
 
 export const Form = styled.form``;
 
-export const Input = styled.input``;
+export const Legend = styled.legend`
+  font-size: 2em;
+`;
+
+export const Input = styled.input`
+  background-color: transparent;
+  border-radius: 50px;
+  display: block;
+  color: #fff;
+  margin: 2em;
+  :active {
+    outline: 0.1em solid ${(props) => props.theme.red};
+  }
+`;
+
+export const Button = styled.button`
+  width: 6em;
+  border-radius: 0.4em;
+  background-color: red;
+  color: #fff;
+  margin-bottom: 1em;
+  padding: 0.25em;
+  cursor: pointer;
+`;
+
+export const Errortext = styled.div`
+  color: red;
+`;
