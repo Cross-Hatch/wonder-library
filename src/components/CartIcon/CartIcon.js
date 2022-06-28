c26a36dd366fab938d8c3c858e37a82c314ddddd
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-06-28 15:15:37 +0000

    Modified CartIcon.js
    - Added link to cart icon. Linked to mycart page

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -1,13 +1,16 @@
-import { Badge, CartImg, Container } from "./CartIcon.style";
-import cartImage from '../../assets/cart-icon.svg'
+import { Badge, CartImg, Container } from './CartIcon.style';
+import cartImage from '../../assets/cart-icon.svg';
+import { Link } from 'react-router-dom';
 
-const CartIcon = ({cartImgWidth}) => {
-    return ( 
-        <Container>
-            <CartImg cartImgWidth={cartImgWidth} src={cartImage} alt="Cart Icon" />
-            <Badge>50</Badge>
-        </Container>
-     );
-}
- 
+const CartIcon = ({ cartImgWidth }) => {
+  return (
+    <Link to="/mycart" style={{marginLeft: "auto"}}>
+      <Container>
+        <CartImg cartImgWidth={cartImgWidth} src={cartImage} alt="Cart Icon" />
+        <Badge>50</Badge>
+      </Container>
+    </Link>
+  );
+};
+
 export default CartIcon;
