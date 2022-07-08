053792bdba97a5fdcd1f3eba26fcb730e20b99de
David Boateng <davebostons2017@gmail.com>
2022-07-08 11:57:24 +0000

    Modified Store.js

--- src/pages/Store/Store.js
+++ src/pages/Store/Store.js
@@ -1,9 +1,25 @@
 import Layout from '../../components/Layout/Layout';
+import ProductCard from '../../components/ProductCard/ProductCard';
+import useProduct from '../../hooks/useProduct';
+import {
+  Featured,
+  Section,
+} from './Store.style'
 
 const Store = () => {
+  const {products} = useProduct({ limit: 8 });
+
   return (
-   <Layout>
-     Store Page
+   <Layout> 
+    <Section> 
+      <Featured> 
+        <div className="featured-products">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+      </Featured>
+    </Section>
    </Layout>
   );
 };
