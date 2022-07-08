66ec1c6ac0277a92bd1ba2d78a70ca8c63740761
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-07-08 03:27:04 +0000

    Modified ProductCartIcon.js
    - Added add to cart functionality

--- src/components/ProductCard/ProductCartIcon/ProductCartIcon.js
+++ src/components/ProductCard/ProductCartIcon/ProductCartIcon.js
@@ -1,9 +1,15 @@
 import CartIcon from '../../../assets/CartIcon';
+import commerce from '../../../lib/commerce';
 import { Container } from './ProductCartIcon.style';
 
-const ProductCartIcon = ({ color }) => {
+const ProductCartIcon = ({ color, id }) => {
+ function addToCart() {
+    commerce.cart.add(id, 1).then((response) => console.log(response));
+    // commerce.cart.refresh().then((cart) => console.log(cart));
+  }
+
   return (
-    <Container>
+    <Container onClick={addToCart} >
       <CartIcon color={color} />
     </Container>
   );
