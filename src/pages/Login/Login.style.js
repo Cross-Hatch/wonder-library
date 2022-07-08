e2e477d4f2af98d03f7a7fa294390d7a92d69bbe
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-07-08 03:36:17 +0000

    Modified Login.style.js
    Added padding to input box

--- src/pages/Login/Login.style.js
+++ src/pages/Login/Login.style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 40vw;
@@ -21,6 +21,7 @@ export const Legend = styled.legend`
 export const Input = styled.input`
   background-color: transparent;
   border-radius: 50px;
+  padding: 0.5em 1em;
   display: block;
   color: #fff;
   margin: 2em;
