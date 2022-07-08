0dd4b868206206d279820b583c4556a5b4a864c3
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-07-08 03:38:03 +0000

    Modified CartItem.js
    CartIems now are displayed according to the contents of the current cart🙌

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,17 +1,27 @@
 import { Container, Wrapper, Description } from './CartItem.style';
 import { stripHtml } from 'string-strip-html';
 import Button from '../Button/Button';
+import { useEffect, useState } from 'react';
+import commerce from '../../lib/commerce';
 
-const CartItem = ({ product }) => {
-  const description = stripHtml(product.description).result;
+const CartItem = ({ product: cartItem }) => {
+  const [productDescription, setProductDescription] = useState('');
+  const description = stripHtml(productDescription).result;
+
+  useEffect(() => {
+    commerce.products.retrieve(cartItem.product_id).then((product) => {
+      setProductDescription(product.description);
+      console.log(product.name)
+    });
+  }, [cartItem.product_id]);
 
   return (
     <Container>
       <Wrapper>
-        <img src={product.image.url} alt={product.name} />
+        <img src={cartItem.image.url} alt={cartItem.name} />
         <Description>
-          <h4>{product.name}</h4>
-          <p>{description ? description : 'error'}</p>
+          <h4>{cartItem.name}</h4>
+          <p>{description ? description : '...'}</p>
           <Button btnName={'Remove'} />
         </Description>
       </Wrapper>
