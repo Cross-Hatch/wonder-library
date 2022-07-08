2dedc83e0fd3d376eef9c74d130aa286b38d6e7e
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-07-08 03:28:43 +0000

    Modified Carticon.js
    - Badge can now display total cart content ❤️

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -1,13 +1,23 @@
 import { Badge, CartImg, Container } from './CartIcon.style';
 import cartImage from '../../assets/cart-icon.svg';
 import { Link } from 'react-router-dom';
+import commerce from '../../lib/commerce';
+import { useEffect, useState } from 'react';
 
 const CartIcon = ({ cartImgWidth }) => {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    commerce.cart.retrieve().then((cart) => {
+      setCart(cart);
+      console.log(cart);
+    });
+  }, []);
+
   return (
-    <Link to="/mycart" style={{marginLeft: "auto"}}>
+    <Link to="/mycart" style={{ marginLeft: 'auto' }}>
       <Container>
         <CartImg cartImgWidth={cartImgWidth} src={cartImage} alt="Cart Icon" />
-        <Badge>50</Badge>
+        <Badge>{cart.total_items}</Badge>
       </Container>
     </Link>
   );
