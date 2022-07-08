a4a81ed67dbaee670c733a4e7283c98a7e38f188
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-07-08 03:25:02 +0000

    Modified Callback.js
    - Changed logic for authentication
    - Added splash screen to stall authentication before redirecting to another page

--- src/pages/Callback/Callback.js
+++ src/pages/Callback/Callback.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import SplashScreen from "../../components/SplashScreen/SplashScreen";
 import commerce from "../../lib/commerce";
 
 const CallBack = () => {
   const location = useLocation();
+  const [loading, setLoading] = useState(true);
   const [jwt, setJwt] = useState(null);
   const [token, setToken] = useState("");
 
@@ -15,24 +17,32 @@ const CallBack = () => {
 
   useEffect(() => {
     commerce.customer.getToken(token, true).then((jwt) => {
-      setJwt(() => jwt.jwt);
-      //   console.log(jwt);
+      setJwt(jwt.jwt);
+      setLoading(false)
+      // console.log(jwt, "use: jwt");
     });
   }, [token]);
+  // console.log(jwt, "info: jwt");
 
-  if (jwt !== null) {
+  if (loading) {
+    return (
+      <SplashScreen />
+    );
+  }
+  else if (localStorage.commercejs_customer_token !== undefined && localStorage.commercejs_customer_token !== null) {
     return (
       <>
         <Navigate replace to="/" />
       </>
     );
+  } else if (localStorage.commercejs_customer_token === undefined) {
+    return (
+      <>
+      {console.log("return")}
+        <Navigate replace to="/login/error/email_auth" />
+      </>
+    );
   }
-
-  return (
-    <>
-      <Navigate replace to="/home?error_email_auth" />
-    </>
-  );
 };
 
 export default CallBack;
